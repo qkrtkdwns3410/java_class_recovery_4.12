7b6aee0e1bba151ae6c35bda1980c7c8ad2790ce
psj <qkrtkdwns3410@naver.com>
2021-04-13 17:48:56 +0900

    4.13

---
+++ src/com/company/Ex3_11.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class Ex3_11 {
+    public static void main(String[] args) {
+        //반올림 -Math.round()
+        //실수를  소수점 첫 째자리에서 반올림한 정수를 반환
+//        long result =Math.round(4.52); //result에 5가 저장된다
+//        double Pi =3.141592;
+//        double shortPi =Math.round(Pi*1000)/1000.0;
+//        System.out.println(shortPi);
+
+//        double pi =3.141592;
+//        System.out.println((int)(pi*1000));
+//        System.out.println((int)(pi*1000)/1000);
+//        System.out.println((int)(pi*1000)/1000.0);
+//
+        
+        
+        //나머지 연산자 %
+        //오른쪽 피연산자로 나누고 남은 나머지를 반환
+        //나누는 피연산자는 0이 아닌 정수만 허용(부호는 무시됨)
+//        int x =10;
+//        int y=8;
+//        System.out.printf("%d을 %d로 나누면, %n",x,y);
+//        System.out.printf("몫은 %d이고, 나머지는 %d입니다\n",x/y,x%y);
+//
+        
+        //3-13비교 연산자
+        //두 피연산자를 비교해서 true 또는 false를 반환
+        
+        //문자열의 비교는 equals()를 사용해야하는 경우도 있음
+//        String str1 = "abc";
+//        String str2 = "abc";
+//        System.out.println(str1 == str2); //true
+//        System.out.println(str1.equals(str2)); //true
+//
+        //
+//        String str1 = new String("abc");
+//        String str2 = new String("abc");
+//        System.out.println(str1==str2); //false
+//        System.out.println(str1.equals(str2));
+        
+        //3-15 논리 연산자
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/com/company/Ex3_9.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Ex3_9 {
+    public static void main(String[] args) {
+        
+        int a = 1_000_000; //1백만 10의 6제곱
+        int b = 2_000_000; //2백만 10의 6제곱
+        
+        //10의 12제곱 int의범위는 10의 9제곱.
+        long c = (long)a * b; //a*b =2,000,000,000,000 ?
+        
+        System.out.println(c);
+        
+        
+    }
+}
