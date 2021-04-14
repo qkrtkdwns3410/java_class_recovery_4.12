606ad6b67c900bcbc4d718a01141eea01867b8ce
psj <qkrtkdwns3410@naver.com>
2021-04-14 16:00:19 +0900

    4.13

--- src/com/company/Ex04_if.java
+++ src/com/company/Ex04_if.java
@@ -32,37 +32,125 @@ public class Ex04_if {
 //            grade='D';
 //        }
 //        System.out.printf("당신의 학점은 "+grade+"입니다.");
+
+//        int score = 0;
+//        char grade = ' ', opt = '0';
+//        System.out.printf("점수를 입력해주세요>>");
+//        Scanner scanner = new Scanner(System.in);
+//        score = scanner.nextInt();
+//
+//        System.out.printf("당신의 점수는 %d입니다\n", score);
+//
+//        if (score >= 90) {
+//            grade = 'A';
+//            if (score >= 98) {
+//                opt = '+';
+//            } else if (score < 94) {
+//                opt = '-';
+//            }
+//        } else if (score >= 80) {
+//            grade = 'B';
+//            if (score >= 88) {
+//                opt = '+';
+//            } else if (score < 84) {
+//                opt = '-';
+//            }
+//        } else {
+//            grade = 'C';
+//        }
+//        System.out.printf("당신의 학점은 %c%c입니다\n", grade, opt);
+//
+//
+
+//        System.out.printf("현재 월을 입력하세요.>");
+//
+//        Scanner scanner =new Scanner(System.in);
+//        int month = scanner.nextInt();  //화면을 통해 입력받은 숫자를 month에 저장합니다
+//
+//        switch (month){
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.printf("현재 계절은 봄입니다");
+//                break;
+//            case 6: case 7:
+//            case 8:
+//                System.out.printf("현재 계절은 여름입니다");
+//                break;
+//            case 9: case 10:
+//            case 11:
+//                System.out.printf("현재의 계절은 가을입니다");
+//            default:
+//            case 12: case 1:  case 2:
+//                System.out.printf("현재의 계절은 겨울입니다");
+//        }
+
+
+//        int num = 0;
+//
+//        //괄호의 내용을 5번 반복합니다
+//        for (int i =1;i<=5;i++){
+//            num = (int) (Math.random() * 6) + 1;
+//            System.out.println(num);
+//        }
+//
+
+//        for (int i=0;i<4;i++){
+//            System.out.println("hello");
+//        }
         
-        int score = 0;
-        char grade = ' ', opt = '0';
-        System.out.printf("점수를 입력해주세요>>");
-        Scanner scanner = new Scanner(System.in);
-        score = scanner.nextInt();
+//        int sum = 0;
+//
+//        for (int i = 1; i < 6; i++) {
+//            sum += i;
+//            System.out.printf("1부터 %2d까지의 합: %2d%n", i, sum);
+//
+//        }
         
-        System.out.printf("당신의 점수는 %d입니다\n", score);
         
-        if (score >= 90) {
-            grade = 'A';
-            if (score >= 98) {
-                opt = '+';
-            } else if (score < 94) {
-                opt = '-';
-            }
-        } else if (score >= 80) {
-            grade = 'B';
-            if (score >= 88) {
-                opt = '+';
-            } else if (score < 84) {
-                opt = '-'
-            }
-        } else {
-            grade = 'C';
-            
+//        for (int i=1;i<6;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println( );;
+//        }
+//
+    
+    
+//        int sum = 0;
+//        int i = 0;
+//
+//        while (sum <= 100) {
+//            System.out.printf("%d - %d\n", i, sum);
+//            sum += ++i;
+//
+//        }
+//        int num = 0, sum = 0;
+//        System.out.printf("숫자를 입력하세요.(예:12345)>>");
+//        Scanner scanner = new Scanner(System.in);
+//        String tmp = scanner.nextLine(); //화면을 통해 입력받은 내용을 tmp에 저장
+//        num = Integer.parseInt(tmp); //입력받은 문자열 (tmp)을 숫자로 변환
+//
+//        while (num != 0) {
+//            //num을 10으로 나눈 나머지를 sum에 더합니다
+//            sum += num % 10;
+//            System.out.printf("sum=%3d num%d\n", sum, num);
+//
+//            num /= 10;
+//
+//        }
+//        System.out.println("각 자리수의 합:"+sum);
+//
+        
+        
+        
+        
+        
+        
         }
-        
-        
     }
-}
+
+
 
 
 
