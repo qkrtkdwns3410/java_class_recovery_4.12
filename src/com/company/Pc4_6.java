ab6467cc636fa7b91426ed7e92878e929277bf86
psj <qkrtkdwns3410@naver.com>
2021-04-14 19:20:29 +0900

    4.13

---
+++ src/com/company/Pc4_6.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Pc4_6 {
+    public static void main(String[] args) {
+        int i = 1, j = 1; //눈
+        
+        for (i = 1; i <= 6; i++) {
+            for (j = 1; j <= 6; j++) {
+                if (i+j ==6){
+                    System.out.printf("%d, %d\n", i, j);
+                }
+                
+            }
+        }
+    }
+}
---
+++ src/com/company/Pc4_7.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Pc4_7 {
+    public static void main(String[] args) {
+    
+    }
+}
