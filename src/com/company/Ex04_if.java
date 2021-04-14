4b952145537242f8a9b95ee6e0d87dfc02a94c5d
psj <qkrtkdwns3410@naver.com>
2021-04-14 18:38:52 +0900

    4.13

--- src/com/company/Ex04_if.java
+++ src/com/company/Ex04_if.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util. *;
 import java.util.Scanner;
 
 public class Ex04_if {
@@ -141,6 +142,98 @@ public class Ex04_if {
 //        }
 //        System.out.println("각 자리수의 합:"+sum);
 //
+    
+//        int input = 0, answer = 0;
+//
+//        answer = (int) (Math.random() * 100) + 1; //1~100사이의 임의의 수를 저장합니다
+//        Scanner scanner = new Scanner(System.in);
+//
+//        do {
+//            System.out.println("1과 100 사이의 정수를 입력하세요>");
+//            input = scanner.nextInt();
+//
+//            if (input > answer) {
+//                System.out.println("더 작은 수로 다시 시도해 보세요.");
+//            } else if (input < answer) {
+//                System.out.println("더 큰 수로 다시 시도해 보세요.");
+//            }
+//        } while (input != answer);
+//
+//        System.out.println("정답입니다.");
+    
+    
+//        int menu = 0;
+//        int num = 0;
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            System.out.println("(1) square");
+//            System.out.println("(2) square root");
+//            System.out.println("(3) log");
+//            System.out.println("원하는 메뉴(1~3)를 선택하세요.(종료:0)>");
+//
+//            String tmp = scanner.nextLine(); //화면에서 입력받은 내용을 tmp에 저장
+//            menu = Integer.parseInt(tmp); //입력받은 문자열(tmp)을 숫자로 변환
+//
+//            if (menu == 0) {
+//                System.out.println("프로그램을 종료합니다");
+//                break;
+//            } else if (!(1 <= menu && menu <= 3)) {
+//                System.out.println("메뉴를 잘못 선택하셨습니다(종료는 0)");
+//                continue;
+//            }
+//            System.out.println("선택하신 메뉴는"+menu+"입니다");
+//        }
+    
+        int menu = 0, num = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        outer: //while문에 outer라는 이름을 붙인다
+        while (true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.println("원하는 메뉴(1~3)를 선택하세요.(종료:0)>");
+    
+            String tmp = scanner.nextLine();
+            menu = Integer.parseInt(tmp);
+    
+            if (menu==0) {
+                System.out.println("프로그램을 종료합니다");
+                break;
+            } else if (!(1 <= menu && menu <= 3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)!");
+                continue;
+            }
+            
+            for(;;){
+                System.out.println("계산할 값을 입력하세요.(계산종료:0, 전체종료:99)>");
+                tmp = scanner.nextLine(); //화면에서 입력받은 내용을 tmp에 저장
+                num = Integer.parseInt(tmp); //입력받은 문자열(tmp)을 숫자로 저장
+    
+                if (num == 0) {
+                    break;
+                }
+    
+                if (num == 99) {
+                    break outer;
+                }
+                switch (menu) {
+                    case 1:
+                        System.out.println("result=" + num * num);
+                        break;
+                    case 2:
+                        System.out.println("result=" + Math.sqrt(num));
+                        break;
+                    case 3:
+                        System.out.println("result=" + Math.log(num));
+                        break;
+                }
+                
+                
+            }
+        }
         
         
         
---
+++ src/com/company/Ex04_practice.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Ex04_practice {
+    public static void main(String[] args){
+        int i = 1;
+        int j = 1;
+        while (i<=10){
+            j = 1;
+            while(j<=i){
+                
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+            
+            
+        }
+    }
+}
