ddd31312b6fde2f1e241eb81a38392b92731a756
psj <qkrtkdwns3410@naver.com>
2021-04-17 01:31:20 +0900

    4.13

---
+++ src/com/company/Ex6/Prac06_01.java
@@ -0,0 +1,15 @@
+package com.company.Ex6;
+//
+//class Student {
+//    String name;
+//    int ban;
+//    int no;
+//    int kor;
+//    int eng;
+//    int math;
+//}
+public class Prac06_01 {
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ src/com/company/Ex6/Prac06_02.java
@@ -0,0 +1,49 @@
+//package com.company.Ex6;
+//
+//public class Prac06_02 {
+//    public static void main(String[] args) {
+//        Student s = new Student("홍길동", 1, 1, 100, 60, 76);
+//
+//        String str = s.info();
+//        System.out.println(str);
+//    }
+//}
+//
+//class Student {
+//    String name;
+//    int ban;
+//    int no;
+//    int kor;
+//    int eng;
+//    int math;
+//
+//    Student(String name, int ban, int no, int kor, int eng, int math) {
+//        this.name = name;
+//        this.ban = ban;
+//        this.no = no;
+//        this.kor = kor;
+//        this.eng = eng;
+//        this.math = math;
+//    }
+//    public String info() {
+//        return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math;
+//
+//
+//
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
