8cb2c519e86b914f3bd02dab6cc338f3dfa15da6
psj <qkrtkdwns3410@naver.com>
2021-05-01 03:21:06 +0900

    4.20

---
+++ src/com/company/ch13/bin/Ex13_6_6.java
@@ -0,0 +1,38 @@
+package com.company.ch13.bin;
+
+public class Ex13_6_6 {
+    public static void main(String[] args) {
+        ThreadEx6_1 th1 = new ThreadEx6_1();
+        ThreadEx6_2 th2 = new ThreadEx6_2();
+    
+        th2.setPriority(7);
+    
+        System.out.println("th1(-)의 우선순위는 " + th1.getPriority());
+        System.out.println("th2(|)의 우선순위는 " + th2.getPriority());
+        th1.start();
+        th2.start();
+        
+    }
+}
+
+class ThreadEx6_1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("-");
+            for (int x = 0; x < 10000000; x++) {
+            
+            }
+        }
+    }
+}
+
+class ThreadEx6_2 extends Thread {
+    public void run() {
+        for (int i = 0; i < 300; i++) {
+            System.out.print("|");
+            for (int x = 0; x < 10000000; x++) {
+            
+            }
+        }
+    }
+}
---
+++ src/com/company/ch13/bin/Ex13_8_8.java
@@ -0,0 +1,4 @@
+package com.company.ch13.bin;
+
+public class Ex13_8_8 {
+}
